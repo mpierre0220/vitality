e2a928a87d26e47a122fd4afc70eb3eb458c4381
mpierre0220 <mpierre@us.ibm.com>
2016-06-17 14:45:15 -0400

    adding the bluemix gp-common project to the repo

--- public/javascript/orgs-dwo.js
+++ public/javascript/orgs-dwo.js
@@ -87,6 +87,7 @@ module.exports = {
     {"name": "ce-store/ce-store", "type": "repo"},
     {"name": "ResilienceTesting/gremlinsdk-python", "type": "repo"},
     {"name": "IBM-Bluemix/docs", "type": "repo"},
-    {"name": "ibm-research/SwiftHLM", "type": "repo"}
+    {"name": "ibm-research/SwiftHLM", "type": "repo"},
+    {"name": "IBM-Bluemix/gp-common", "type": "repo"}
   ]
 };
